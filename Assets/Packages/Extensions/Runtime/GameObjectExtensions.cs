e9caecba9843fe93418e4e50c1a00cb394518d61
HankunYu <KmcyD1@gmail.com>
2025-03-13 22:25:26 +0000

    Update GameObjectExtensions.cs

--- Assets/Packages/Extensions/Runtime/GameObjectExtensions.cs
+++ Assets/Packages/Extensions/Runtime/GameObjectExtensions.cs
@@ -22,4 +22,24 @@ public static class GameObjectExtensions
         }
         coroutine = monoBehaviour.StartCoroutine(routine);
     }
+
+    /// <summary>
+    /// Gets a descendant GameObject with a specific name
+    /// </summary>
+    /// <param name="go">The parent object that is searched for a named child.</param>
+    /// <param name="name">Name of child to be found.</param>
+    /// <returns>The returned child GameObject or null if no child is found.</returns>
+    // From Unity.XR.CoreUtils
+    public static GameObject GetNamedChild(this GameObject go, string name)
+    {
+        k_Transforms.Clear();
+        go.GetComponentsInChildren(k_Transforms);
+        var foundObject = k_Transforms.Find(currentTransform => currentTransform.name == name);
+        k_Transforms.Clear();
+
+        if (foundObject != null)
+            return foundObject.gameObject;
+
+        return null;
+    }
 }
